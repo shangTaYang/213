43c73dcc6438590765c6c2233c4fc06a6d7f32fb
shangTaYang <105040810+shangTaYang@users.noreply.github.com>
2023-10-25 09:00:37 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,64 +1,18 @@
-import lineTool
-import os
-import requests
-from datetime import datetime, timedelta, timezone, date
-import re
-
-chubao = r'''((出包)|(搞笑)|(失誤)|(滑稽)|(錯誤)|(挑戰)|(失敗)|(趣味)|
-(尷尬)|(瞬間)|(爆笑)|(惡作劇)).*?'''
-
-class YoutubeSpider():
-    def __init__(self, api_key):
-        self.base_url = "https://www.googleapis.com/youtube/v3/"
-        self.api_key = api_key
-
-    def search_videos(self, query, published_after):
-        """使用關鍵字搜尋影片"""
-        path = f"search?part=snippet&maxResults=50&q={query}&type=video&videoDefinition=high&publishedAfter={published_after}&key={self.api_key}"
-        data = self.get_html_to_json(path)
-        return data
-
-    def search_channel(self, channel_id):
-        path = f"channels?part=statistics&id={channel_id}&key={self.api_key}"
-        data = self.get_html_to_json(path)
-        return data
-
-    def get_html_to_json(self, path):
-        """組合 URL 後 GET 網頁並轉換成 JSON"""
-        api_url = f"{self.base_url}{path}"
-        r = requests.get(api_url)
-        if r.status_code == requests.codes.ok:
-            data = r.json()
-        else:
-            data = None
-        return data
-
-
-now = datetime.utcnow().replace(tzinfo=timezone.utc)
-past_hour = timedelta(hours=1)
-published_after = (now - past_hour).strftime("%Y-%m-%dT%H:%M:%SZ")
-
-# 初始化爬蟲物件並搜尋符合條件的影片
-spider = YoutubeSpider(os.getenv('API_KEY'))
-query = "出包"
-data = spider.search_videos(query, published_after)
-car_accident_list = []
-# 輸出所有影片的標題
-
-for item in data["items"]:
-    #     lineTool.lineNotify(os.getenv('token'),item)
-    dic = {}
-    dic["title"] = item["snippet"]["title"]
-    video_url_id = item["id"]["videoId"]
-    dic["video_url"] = f"https://www.youtube.com/watch?v={video_url_id}"
-    dic["author"] = item["snippet"]["channelTitle"]
-    channel_id = item["snippet"]["channelId"]
-    channel_data = spider.search_channel(channel_id)
-    # subscriberCount = channel_data["items"][0]["statistics"]["subscriberCount"]
-    # if int(subscriberCount) < 2000:
-    if re.search(chubao, dic["title"]):
-        car_accident_list.append(dic)
-
-
-car_massege = f"出包{car_accident_list}"
-lineTool.lineNotify(os.getenv('token'), car_massege)
+import feedparser
+from datetime import datetime, timedelta, timezone,date
+# RSS feed的URL
+def search_no():
+    rss_url = "https://trends.google.com.tw/trends/trendingsearches/daily/rss?geo=TW"
+    
+    # 使用feedparser库来解析RSS数据
+    feed = feedparser.parse(rss_url)
+    
+    # 遍历并提取每个条目的标题和链接
+    now = datetime.now(
+        tz=timezone(timedelta(hours=8)))
+    now_all = now.strftime('%Y/%m/%d ')
+    print(now_all+"熱搜排行")
+    for index, entry in enumerate(feed.entries):
+      if index <5:
+        # print(f'第{index+1}名:{entry.title}\n相關新聞:{entry.ht_news_item_title}',end='\n\n')
+        lineTool.lineNotify(os.getenv('token'), f'第{index+1}名:{entry.title}\n相關新聞:{entry.ht_news_item_title}',end='\n\n')
