205872fbcf77b44de7e85236db1fc4b8bc9477de
shangTaYang <105040810+shangTaYang@users.noreply.github.com>
2023-10-27 01:28:40 +0800

    Update run.py

--- run.py
+++ run.py
@@ -7,35 +7,6 @@ import feedparser
 from bs4 import BeautifulSoup
 
 
-
-
-def google_news():
-    now = datetime.now(
-        tz=timezone(timedelta(hours=8)))
-    now_all = now.strftime('%Y/%m/%d ')
-    headers = {
-        'user-agent':
-        'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36 Edg/107.0.1418.62',
-    }
-
-    # 發出網路請求 GET
-    resp = requests.get(
-        'https://news.google.com/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JYcG9MVlJYR2dKVVZ5Z0FQAQ?hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant',
-        headers=headers)
-
-    # 使用 BeautifulSoup 解析內容為 BeautifulSoup 物件
-    soup = BeautifulSoup(resp.text, "html.parser")
-    # 解析抓取 class name 為 NiLAwe 區塊（每一個新聞組合）
-    rows = soup.select("c-wiz div c-wiz h4")
-    asd = "\n\n"
-    news = [now_all + " 重點新聞" + asd]
-    for i in rows:
-        aaa = i.text
-        ddd = " "
-        news.append(aaa + ddd + asd)
-    del news[11:]
-    
-    lineTool.lineNotify(os.getenv('token'), news)
        
 def get_ettoday_social_news_url(url):
   headers = {
@@ -104,7 +75,7 @@ def International_news_job():
         sbb = title + url 
         lineTool.lineNotify(os.getenv('token'), sbb)
 
-chubao = r'''((出包)|(糗)|(笑死)|(丟臉)|(尷尬)|(錯誤)|(出糗)|(失誤)|'''
+chubao = r'''((出包)|(糗)|(笑死)|(丟臉)|(尷尬)|(錯誤)|(出糗)|(失誤)|(安全))'''
 
   
 today = date.today()
